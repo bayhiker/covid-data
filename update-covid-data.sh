6c044a7a28321e72b80c5e680092716049625263
bayhiker <bayhiker@users.noreply.github.com>
2020-04-08 20:44:26 -0700

    Add -f option for upate-covid-data.sh

--- update-covid-data.sh
+++ update-covid-data.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+FORCE=0
+
+while getopts ":f" opt; do
+    case ${opt} in 
+        f )  FORCE=1
+            ;;
+        \? ) echo "Usage: update-covid-data.sh [-f]"
+            ;;
+    esac
+done
+
 COVID_DATA_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)"
 COVID_DIR="$COVID_DATA_DIR/../covid"
 COVID_DATA_SOURCES_DIR="$COVID_DATA_DIR/../covid-data-sources"
@@ -9,8 +20,13 @@ echo "Current time is: $(date)"
 
 cd $COVID_DATA_SOURCES_DIR_JHU
 if /usr/bin/git pull | grep -q 'Already up to date'; then
-    echo 'Data is up-to-date, nothing to be done'
-    exit 0
+    echo 'Data is up-to-date'
+    if [ "${FORCE}" -eq "0" ]; then
+        echo 'Nothing to be done'
+        exit 0
+    else
+        echo "Forcing data update"
+    fi
 fi
 cd $COVID_DATA_DIR
 source ~/.profile
